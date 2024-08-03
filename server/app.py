02438f279f468a0ec11aaf605743443040211ba8
kvn <kvnareba@gmail.com>
2024-08-03 16:06:57 +0300

    add app file

--- server/app.py
+++ server/app.py
@@ -3,13 +3,24 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, jsonify
 from flask_restful import Resource
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 # Local imports
 from config import app, db, api
+
 # Add your model imports
 
+from models import db, User, Customer, Seller, Product, Cart, Order
+
+
+
+db.init_app(app)
+migrate = Migrate(app, db)
+jwt = JWTManager(app)
 
 # Views go here!
 
